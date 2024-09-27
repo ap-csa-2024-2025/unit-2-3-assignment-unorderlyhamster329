c3e1a2495a297e5475b70b86065c6de29181e246
unorderlyhamster329 <mattng08@gmail.com>
2024-09-27 10:53:37 -0700

    wdwqdwq

--- Classwork.java
+++ Classwork.java
@@ -3,6 +3,22 @@ public class Classwork
   public static void main(String[] args)
   {
     // test your code here
+    
+  }
+
+  public static String firstLastN(String word, int n) {
+    String output = new String();
+    String firstN = word.substring(0, n); // first n letters; substring from 0 to n-1
+    int indexofNToLast = word.length() - n; //index of the nth to last letter
+    String lastN = word.substring(indexofNToLast); // last n letters, starting at nth to last letter
+
+    output = firstN + lastN;
+    return output; 
+  }
+
+  public static String delEnd(String bord, int b) {
+    String output = new String();
+    int b = world.length() - b;
   }
 
   // write solutions to problems below
